24e441279a0504ee2f11a9b1b200046d1e22c189
mana <wasemikanm@yahoo.co.jp>
2024-10-07 14:42:03 +0900

    first commit

---
+++ src/test/java/com/example/pictgram/Pictgram3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pictgram3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
